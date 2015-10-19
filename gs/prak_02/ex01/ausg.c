0e738b8c22625ca173fbc7d4e184c885812c31c1
kafawi <kafa.witte@web.de>
2015-10-19 22:20:49 +0200

    Update ausg.c

--- gs/prak_02/ex01/ausg.c
+++ gs/prak_02/ex01/ausg.c
@@ -3,18 +3,18 @@
 #include <string.h>
 #include "tft.h"
 
-static bool oefters_ausgefuehrt;
+static bool oefters_ausgefuehrt = false;
 int start;
 int ende;
 int anz_prim;
 
 void print_ergebnis_TFT(){
-	bool oefters_ausgefuehrt;
 	char ausgabe[80];
 	
 	if (!oefters_ausgefuehrt){
 		Init_TI_Board();
 		TFT_cls();
+		oefters_ausgefuehrt = true;	
 	}
 	
 	sprintf(ausgabe, "Primzahlenanzahl zwischen %i und %i: %i \n", start, ende, anz_prim );
