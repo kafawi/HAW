5e1419ac9f816b3d638e69f5cb5c80ffada5ee53
kafawi <kafa.witte@web.de>
2015-10-19 21:25:27 +0200

    Update sieb.h

--- gs/prak_02/ex01/sieb.h
+++ gs/prak_02/ex01/sieb.h
@@ -1,5 +1,11 @@
 // --------------------------------------------------------------- KONSTANTEN -
 #define ANZ 100
 // --------------------------------------------------------------- PROTOTYPEN -
+void init_prim_arr()
+void calc_prim_arr()
+int calc_sum_prim(int start, int ende)
+
+
+
 extern char prim_arr[ANZ];
 // ---------------------------------------------------------------------- EOF -
